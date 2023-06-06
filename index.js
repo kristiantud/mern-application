7bfd8406ab6fa15e7e289117143bf41a0608bb28
Kristian Tud <kristiantud@hotmail.com>
2023-06-05 22:16:06 -0600

    added dependencies/packages, added import statements

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "corse";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import  { fileURLToPath } from "url";
+
