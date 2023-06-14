8dad234f897d050948d35c263d1ac15fee658910
Kristian Tud <kristiantud@hotmail.com>
2023-06-13 20:01:53 -0600

    finished middleware authorization

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,3 +40,26 @@ export const register = async (req,res) => {
         res.status(500).json({error: error.message});
     }
 };
+
+// LOGGING IN
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await user.findOne({email: email});
+
+        if (!user){
+            return res.status(400).json({ msg: "User doesn't exist."});
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) return res.status(400).json({ msg: "Invalid credentials"});
+
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            delete user.password;
+            res.status(200).json({ token, user })
+        }
+    } catch(error){
+        res.status(500).json({error: error.message});
+    }
+}
+
+
