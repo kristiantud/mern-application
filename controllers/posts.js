4652bc05684464253e97eb92597da85d9be9ef2b
Kristian Tud <kristiantud@hotmail.com>
2023-06-14 03:40:26 -0600

    finished posts, finished the backend (mostly)

---
+++ controllers/posts.js
@@ -0,0 +1,75 @@
+import Post from "../models/Post.js";
+
+
+// CREATE
+export const createPost = async (req, res) => {
+    try{
+        const { userId, description, picturePath } = req.body;
+        const user = await User.findById(userId);
+        const newPost = new Post({
+            userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            location: user.location,
+            description,
+            userPicturePath: user.picturePath,
+            postPicturePath,
+            likes: {},
+            comments: []
+        })
+
+        await newPost.save();
+        const post = await Post.find();
+
+        res.status(201).json(post); // updates newsfeed with this post added
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+} 
+
+// READ
+export const getFeedPosts = async (req,res) => {
+    try {
+        
+        const post = await Post.find();
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getUserPost = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const post = await Post.find( { userId });
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+// UPDATE
+export const likePost = async (req,res) => {
+    try {
+        const { id } = req.params;
+        const { userId } = req.body;
+        const post = await Post.findById(id);
+        const isLiked = post.likes.get(userId); // likes is a map that grabs a userid
+
+        if (isLiked){
+            post.likes.delete(userId);
+        } else {
+            post.likes.set(userId, true); // adds into likes, which is a map
+        }
+
+        const updatedPost = await Post.findByIdAndUpdate(
+            id,
+            { likes: post.likes },
+            { new: true }, 
+        )
+
+        res.status(200).json(updatedPost);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
