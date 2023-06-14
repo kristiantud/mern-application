dccfbc86d92247d193a6c1476737e39059a72f87
Kristian Tud <kristiantud@hotmail.com>
2023-06-13 20:24:07 -0600

    finished setting up users.js for routes

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import  { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js"; 
 
 /* CONFIGURATIONS  */
@@ -39,6 +40,10 @@ const upload = multer({storage});
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register); 
+
+// ROUTES
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 
 // MONGOOSE
