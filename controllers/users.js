021f36517bd15c69bbc17332b6a012651aa047b9
Kristian Tud <kristiantud@hotmail.com>
2023-06-14 02:00:23 -0600

    finished adding routers for posts

--- controllers/users.js
+++ controllers/users.js
@@ -28,6 +28,36 @@ export const getUserFriends = async(req,res) => {
         );
         res.status(200).json(formattedFriends);
     } catch (error) {
+        res.status(404).json({message: error.message});
+    } 
+}
 
+// UPDATE
+export const addRemoveFriend = async (req,res) => {
+    try {
+        const { id, friendsId } = req.params;
+        const user = await User.findById(id);
+        const friend = await User.findById(friendId);
+
+        if (user.friends.includes(friendId)){
+            user.friends = user.friends.filter((id) => id !== friendId)
+            friend.friends = friend.friends.filter((id) => id !== id);
+        } else {
+            user.friends.push(friendId);
+            friend.friends.push(id);
+        }
+
+        await user.save();
+        await friend.save();
+        
+        const formattedFriends = friends.map(
+            ({ _id, firstName, lastName, occupation, location, picturePath }) => { 
+            return { _id, firstName, lastName, occupation, location, picturePath };
+            }
+        );
+        res.status(200).json(formattedFriends);
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
     } 
 }
---
+++ routes/posts.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { getFeedPosts, getUserPosts, likePosts } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+// READ
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+
+// UPDATE
+router.patch("/:id/like", verifyToken, likePosts);
