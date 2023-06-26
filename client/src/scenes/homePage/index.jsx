04cd8c9e6346a9d7858fdeeea87041ef236372e5
Kristian Tud <kristiantud@hotmail.com>
2023-06-26 17:24:34 -0600

    finished profile widget for homepage

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,9 +1,38 @@
+import { Box,useMediaQuery } from "@mui/material";
 import Navbar from "scenes/navbar";
-
+import UserWidget from "scenes/widgets/UserWidgets"
 
 const HomePage = () => {
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+    const { _id, picturePath } = useSelector((state) => state.user);
+
+
     return (
-        <Navbar></Navbar>
+        <Box>
+            <Navbar />
+            <Box
+                width="100%"
+                padding="2rem 6%"
+                display={isNonMobileScreens ? "flex" : "block"}
+                gap="0.5rem"
+                justifyContent="space-between"
+            >
+                <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+                    <UserWidget userId={_id} picturePath={picturePath} />
+                </Box>
+                <Box
+                    flexBasis={isNonMobileScreens ? "42%" : undefined}
+                    mt={isNonMobileScreens ? undefined : "2rem"}
+                >
+
+                </Box>
+                {isNonMobileScreens && (
+                    <Box flexBasis="26%"></Box>
+                )}
+            </Box>
+        </Box>
+        
+
     )
 };
 
