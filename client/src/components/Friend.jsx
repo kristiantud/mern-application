5d05b1c969f3382c33cd21e6f53ff13b7d4510d9
Kristian Tud <kristiantud@hotmail.com>
2023-07-08 15:10:16 -0600

    friend adding/removing now works properly

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -26,11 +26,13 @@ const Friend = ({ friendId, name,subtitle, userPicturePath}) => {
 
     const isFriend = friends.find((friend) => friend._id === friendId);
     const patchFriend = async () => {
+        
         const response = await fetch(`http://localhost:3001/users/${_id}/${friendId}`, 
         {
             method: "PATCH",
             headers: 
-            { Authorization: `Bearer ${token}`,
+            { 
+                Authorization: `Bearer ${token}`,
                 "Content-Type": "application/json"
             }
         })
--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -43,7 +43,7 @@ const PostsWidget = ({ userId, isProfile = false}) => {
         <>
             {/* {console.log("contents of posts: " + Object.values(posts[0]))} */}
             {/* {console.log(postsAsArray)} */}
-            {/* {console.log(posts)} */}
+            {console.log(posts)}
             {posts.map(
                 ({
                     _id,
