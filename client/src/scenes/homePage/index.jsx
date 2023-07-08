4570b518303482729fc8e951567cedc70a355cbc
Kristian Tud <kristiantud@hotmail.com>
2023-07-08 14:39:06 -0600

    finished friendswidget and advertwidget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,6 +4,8 @@ import UserWidget from "scenes/widgets/UserWidgets"
 import { useSelector } from "react-redux";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () => {
     const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -35,7 +37,9 @@ const HomePage = () => {
                 </Box>
                 {isNonMobileScreens && (
                     <Box flexBasis="26%">
-                        <h3> Hello World</h3>
+                        <AdvertWidget/>
+                        <Box m="2rem 0"/>
+                        <FriendListWidget userId={_id} />
                     </Box>
                 )}
             </Box>
