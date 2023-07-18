24e85d387742740a83bd83694c77bfb6fa25e1d2
Kristian Tud <kristiantud@hotmail.com>
2023-07-17 22:39:23 -0600

    notifications now successfully getting fetched without any bugs

--- client/src/scenes/notifPage/index.jsx
+++ client/src/scenes/notifPage/index.jsx
@@ -4,48 +4,59 @@ import { Box } from "@mui/material";
 import Navbar from "scenes/navbar";
 import NotifsWidget from "scenes/widgets/NotifsWidget";
 
+
 // this will fetch all of the notifications that the user gets from interactions with other people
 const Notifications = ({}) => {
 
     const token = useSelector((state) => state.token);
     const { _id } = useSelector((state) => state.user);
-    let [notifications, setNotifications] = useState("none");
+    let [notifications, setNotifications] = useState(null);
+    let [ dataReturned, setDataReturned ] = useState(false); 
     // const notifications = useSelector((state) => state.user.notifications);
 
-    
+    const getNotifs = async () => {
+        const response = await fetch (`http://localhost:3001/users/${_id}/notifications`,{
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}`}
+        })
 
+        const data = await response.json();
+        setNotifications(data);
+        
+        setDataReturned(true);
+
+        
+        // const data = await response.json();
+        // setNotifications(data);  
+    }
+
+        
+    
     useEffect(() => {
-        const getNotifs = async () => {
-            const response = await fetch (`http://localhost:3001/users/${_id}/notifications`,{
-                method: "GET",
-                headers: { Authorization: `Bearer ${token}`}
-            })
-    
-            const data = await response.json();
-            setNotifications(data);
-            
-            
-            // const data = await response.json();
-            // setNotifications(data);  
-        }
         getNotifs();
-      }, []); //eslint-disable-ine react-hooks/exhaustive-deps
+    }, [])
+    
 
 
+    
     return (
         <>
             <Navbar />
             <Box>
-                {console.log(notifications)}
-                {notifications.map(
-                    (notification) => (
-                        
-                        <h3>{notification}</h3>
+                { dataReturned ? (
+                    notifications.map(
+                        (notification) => (                        
+                            <h3>{notification}</h3>
+                        )
                     )
+                ) : (
+                    <h3>data is loading...</h3>
                 )}
             </Box>
         </>
     )
+    
+    
 
 
 }
