960737cf89f4cab95060acf0f20332e30375f18d
Kristian Tud <kristiantud@hotmail.com>
2023-08-08 19:18:11 -0600

    finished the fetch part but we have to move this part to the parent - MessagesWidget

--- client/src/scenes/widgets/MsgPreviewWidget.jsx
+++ client/src/scenes/widgets/MsgPreviewWidget.jsx
@@ -3,10 +3,12 @@ import { useNavigate } from "react-router-dom";
 import MsgPreviews from "./MsgPreviews";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 
 // fetches the messages to populate the preview list
 const MsgPreviewWidget = ({}) => {
+    const token = useSelector((state) => state.token);
     const msgs = [1,2,3,4,5,6,7]; // this will be replaced by msgs stored in the user (within the state) when the user first logs in
     const [selectedMsg, setSelectedMsg] = useState(-1);
     const navigate = useNavigate();
@@ -16,6 +18,17 @@ const MsgPreviewWidget = ({}) => {
     
     if (messageId !== undefined){
         // fetch the message content from the server
+        const getMessage = async () => {
+            const response = await fetch(`http://localhost:3001/messages/${messageId}`, {
+                method: "GET", 
+                headers: {
+                    Authorization: `Bearer ${token}`
+
+                }
+            })
+        }
+
+        getMessage();
     }
 
     
