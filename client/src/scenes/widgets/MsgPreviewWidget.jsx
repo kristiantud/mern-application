b322ba8b8ce1e72446e68b9d62b7eecbde0fdaf9
Kristian Tud <kristiantud@hotmail.com>
2023-08-08 18:45:34 -0600

    starting to create the fetch call to the server for the messages

--- client/src/scenes/widgets/MsgPreviewWidget.jsx
+++ client/src/scenes/widgets/MsgPreviewWidget.jsx
@@ -2,13 +2,21 @@
 import { useNavigate } from "react-router-dom";
 import MsgPreviews from "./MsgPreviews";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 // fetches the messages to populate the preview list
 const MsgPreviewWidget = ({}) => {
-    const msgs = [1,2,3,4,5,6,7];
+    const msgs = [1,2,3,4,5,6,7]; // this will be replaced by msgs stored in the user (within the state) when the user first logs in
     const [selectedMsg, setSelectedMsg] = useState(-1);
     const navigate = useNavigate();
+    const { messageId } = useParams();
+
+    console.log(messageId);
+    
+    if (messageId !== undefined){
+        // fetch the message content from the server
+    }
 
     
 
